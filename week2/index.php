29f5e2cbea06d09ceec37a597a9c322cec9845be
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 14:24:42 +0800

    final

--- week2/index.php
+++ week2/index.php
@@ -8,9 +8,11 @@
 <body>
 
 <?php
-echo "My first PHP script!";
-echo date("d M Y");
-?>
-
+date_default_timezone_set("Asia/Kuala_Lumpur");
+echo "My first PHP script!";?>
+<br>
+<?php echo date("d M Y");?>
+<br>
+<?php echo date("g:i A ");?>
 </body>
 </html>
