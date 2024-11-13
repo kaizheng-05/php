ab320ece10e35fc9f32a3fa63d6dca516df0016d
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 14:48:37 +0800

    decorate

--- week2/index.php
+++ week2/index.php
@@ -3,6 +3,8 @@
 <head>
   <style>
      body {background-color: magenta;}
+     .date{color:aqua;}
+     .time{color:crimson;}
   </style>
 </head>
 <body>
@@ -10,9 +12,7 @@
 <?php
 date_default_timezone_set("Asia/Kuala_Lumpur");
 echo "My first PHP script!";?>
-<br>
-<?php echo date("d M Y");?>
-<br>
-<?php echo date("g:i A ");?>
+<h1 class= date><?php echo date("d M Y");?></h1>
+<h1 class= time><?php echo date("g:i A");?></h1>
 </body>
 </html>
