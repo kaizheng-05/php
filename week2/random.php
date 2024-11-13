a4ca9e050cd0d9efb13f8f8f86a30854fc61f499
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 15:06:14 +0800

    change size

--- week2/random.php
+++ week2/random.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <style>
-        .q1{color:green; font-style: italic;}
-        .q2{font-style: italic; color: blue;}
-        .q3{font-weight: bold; color: red;}
-        .q4{font-style: italic; font-weight: bold;}
+        .q1{color:green; font-style: italic; font-size: 100px;}
+        .q2{font-style: italic; color: blue; font-size: 100px;}
+        .q3{font-weight: bold; color: red; font-size: 100px;}
+        .q4{font-style: italic; font-weight: bold; font-size: 100px;}
     </style>
     <body>
         <p class=q1><?php echo(rand(100,200));?></p>
