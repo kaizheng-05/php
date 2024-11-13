2f56e78175b11c88e0ce6e3fbcff955f3a67597a
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 15:03:38 +0800

    exercise

---
+++ week2/random.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .q1{color:green; font-style: italic;}
+        .q2{font-style: italic; color: blue;}
+        .q3{font-weight: bold; color: red;}
+        .q4{font-style: italic; font-weight: bold;}
+    </style>
+    <body>
+        <p class=q1><?php echo(rand(100,200));?></p>
+        <p class=q2><?php echo(rand(100,200));?></p>
+        <p class=q3><?php echo(rand(100,200));?></p>
+        <p class=q4><?php echo(rand(100,200));?></p>
+    </body>
+</html>
