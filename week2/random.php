13c21bec76e3e3d77981fc23df630c8b188bcd99
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 15:20:54 +0800

    final change

--- week2/random.php
+++ week2/random.php
@@ -7,9 +7,13 @@
         .q4{font-style: italic; font-weight: bold; font-size: 70px;}
     </style>
     <body>
-        <p class=q1><?php echo(rand(100,200));?></p>
-        <p class=q2><?php echo(rand(100,200));?></p>
-        <p class=q3><?php echo(rand(100,200));?></p>
-        <p class=q4><?php echo(rand(100,200));?></p>
+        <?php 
+        $num1=(rand(100,200));
+        $num2=(rand(100,200));
+        ?>
+        <p class=q1><?php echo $num1 ?></p>
+        <p class=q2><?php echo $num2 ?></p>
+        <p class=q3><?php echo $num1+$num2 ?></p>
+        <p class=q4><?php echo $num1*$num2 ?></p>
     </body>
 </html>
