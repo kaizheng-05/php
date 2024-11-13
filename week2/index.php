57eefdbc02d6fd34f040880e11ad681ec54d103f
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-13 13:48:14 +0800

    proper code

---
+++ week2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+     body {background-color: black;}
+  </style>
+</head>
+<body>
+
+<?php
+echo "My first PHP script!";
+echo date("1");
+?>
+
+</body>
+</html>
