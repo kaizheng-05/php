9b988593d320637f67f7134bac0e18b5bec704f5
kai zheng <leekaizheng0905@e.neivce.edu.my>
2024-11-27 15:28:45 +0800

    merge into one

--- week4/form_handle.php
+++ week4/form_handle.php
@@ -1,8 +1,16 @@
+<?php 
+if(!empty($_POST))
+{
+    echo $_POST["name"]."<br>";
+    echo $_POST["email"]."<br>";
+    echo $_POST["age"]."<br>";
+}
+?>
 <!DOCTYPE HTML>
 <html>  
   <body>
 
-    <form action="form_validate.php"  method="post">
+    <form action="<?php echo $_SERVER["PHP_SELF"]?>"  method="post">
     Name: <input type="text"name="name"><br>
     E-mail: <input type="text"name="email"><br>
     age: <input type="text"name="age"><br>
--- week4/form_validate.php
+++ week4/form_validate.php
@@ -1,23 +1,30 @@
+<?php 
+if(!empty($_POST))
+{
+  echo $_POST["name"]."<br>";
+  $mail= $_POST["email"];
+    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+      echo "$mail is a valid email address"."<br>";
+    } 
+    else {
+      echo "$mail is not a valid email address"."<br>";
+    }
+  $age=$_POST["age"];
+    if ($age >= 18 && $age < 100){
+      echo "$age is a numeric value"."<br>";
+    }
+    else{
+      echo "$age is not a numeric value"."<br>";
+    } 
+}?>
 <!DOCTYPE HTML>
 <html>  
   <body>
-
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your Email: <?php $mail= $_POST["email"];
-    if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-      echo "$mail is a valid email address";
-    } 
-    else {
-      echo "$mail is not a valid email address";
-    }
-     ?><br>
-    age: <?php $age=$_POST["age"];
-    if ($age >= 18 && $age < 100){
-      echo "$age is a numeric value";
-    }
-    else{
-      echo "$age is not a numeric value";
-    }?>
-    <br>
+  <form action="<?php echo $_SERVER["PHP_SELF"]?>"  method="post">
+    Name: <input type="text"name="name"><br>
+    E-mail: <input type="text"name="email"><br>
+    age: <input type="text"name="age"><br>
+<input type="submit">
+    </form>
   </body>
 </html>
