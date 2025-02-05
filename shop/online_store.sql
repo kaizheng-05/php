851dc7c636d37c45f75335612c80e0876ca09dc3
kai zheng <leekaizheng0905@e.neivce.edu.my>
2025-02-05 15:39:57 +0800

    change

--- shop/online_store.sql
+++ shop/online_store.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 05, 2025 at 02:37 AM
--- Server version: 8.3.0
--- PHP Version: 8.2.18
+-- Generation Time: Feb 05, 2025 at 07:39 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
   `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `password` int NOT NULL,
+  `password` int(11) NOT NULL,
   `firstname` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `lastname` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `gender` enum('male','female') NOT NULL,
@@ -55,10 +55,10 @@ INSERT INTO `customer` (`email`, `password`, `firstname`, `lastname`, `gender`, 
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `description` text NOT NULL,
-  `product_cat` int NOT NULL,
+  `product_cat` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `price` double NOT NULL,
   `promotion_price` double NOT NULL,
   `manufacture_date` date DEFAULT NULL,
@@ -71,13 +71,13 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `product_cat`, `price`, `promotion_price`, `manufacture_date`, `expired_date`) VALUES
-(1, 'Basketball', 'A ball used in the NBA.', 1, 49.99, 0, NULL, NULL),
-(3, 'Gatorade', 'This is a very good drink for athletes.', 3, 1.99, 0, NULL, NULL),
-(4, 'Eye Glasses', 'It will make you read better.', 2, 6, 0, NULL, NULL),
-(5, 'Trash Can', 'It will help you maintain cleanliness.', 2, 3.95, 0, NULL, NULL),
-(6, 'Mouse', 'Very useful if you love your computer.', 2, 11.35, 0, NULL, NULL),
-(7, 'Earphone', 'You need this one if you love music.', 2, 7, 0, NULL, NULL),
-(8, 'Pillow', 'Sleeping well is important.', 2, 8.99, 0, NULL, NULL);
+(1, 'ps controller', 'A device that used with ps5', '2', 299.99, 200, '2023-04-29', '2025-02-05'),
+(3, 'Gatorade', 'This is a very good drink for athletes.', '3', 1.99, 0, NULL, NULL),
+(4, 'Eye Glasses', 'It will make you read better.', '2', 6, 0, NULL, NULL),
+(5, 'Trash Can', 'It will help you maintain cleanliness.', '2', 3.95, 0, NULL, NULL),
+(6, 'Mouse', 'Very useful if you love your computer.', '2', 11.35, 0, NULL, NULL),
+(7, 'Earphone', 'You need this one if you love music.', '2', 7, 0, NULL, NULL),
+(8, 'Pillowsss', 'Sleeping well is important.', '2', 8.99, 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -87,8 +87,8 @@ INSERT INTO `products` (`id`, `name`, `description`, `product_cat`, `price`, `pr
 
 DROP TABLE IF EXISTS `product_cat`;
 CREATE TABLE IF NOT EXISTS `product_cat` (
-  `product_cat_id` int NOT NULL,
-  `product_cat_name` varchar(100) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `product_cat_id` int(11) NOT NULL,
+  `product_cat_name` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `product_cat_description` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`product_cat_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `product_cat` (
 
 INSERT INTO `product_cat` (`product_cat_id`, `product_cat_name`, `product_cat_description`) VALUES
 (1, 'sport', ''),
-(2, 'acessories', ''),
+(2, 'accessories ', ''),
 (3, 'drinks', '');
 COMMIT;
 
